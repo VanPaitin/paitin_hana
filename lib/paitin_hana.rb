2acde37079191f569d8e96d3420a3011c05ce7c9
Mayowa Pitan <mayowa.pitan@andela.com>
2016-06-10 00:56:59 +0100

    feat (views): implement the views functionality

--- lib/paitin_hana.rb
+++ lib/paitin_hana.rb
@@ -1,9 +1,17 @@
 require "paitin_hana/version"
+require "paitin_hana/base_controller"
+require "pry"
+require "paitin_hana/utilities"
+require "paitin_hana/dependencies"
+require "paitin_hana/routing/router"
 
 module PaitinHana
   class Application
     def call(env)
-      [200, {}, ["I respond to all request"]]
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [500, {}, []]
+      end
+      get_rack_app(env).call(env)
     end
   end
 end
